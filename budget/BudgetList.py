1b00882f31bb96e1676c8729a22e8f1e45afd668
dilip <dilipm@yahoo.com>
2020-06-24 10:02:37 -0400

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -27,7 +27,7 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-    print('The count of all expenses:', len(myBudgetList))
+    print('The count of all expenses: ', len(myBudgetList))
 
 
 if __name__ == '__main__':
